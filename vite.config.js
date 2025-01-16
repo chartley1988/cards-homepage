b32aaf9e552ceabe9916000faf8391855ea53ced
Chartley <chartley1988@gmail.com>
2025-01-16 12:16:15 -0700

    fixes issue where docs url requires trailing forward slash
    previously, /docs/ would work, but /docs would not

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,15 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	server: {
 		proxy: {
-			"/docs": {
-				target: "http://localhost:5174", // Default Vitepress port
+			"^/docs$": {
+				target: "http://localhost:5174",
 				changeOrigin: true,
+				rewrite: () => "/",
+			},
+			"^/docs/": {
+				target: "http://localhost:5174",
+				changeOrigin: true,
+				// rewrite: (path) => path.replace(/^\/docs/, ""),
 			},
 		},
 	},
