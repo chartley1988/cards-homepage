ee1ba33a04f81c0f497e78cea8a077514e2343a4
Chartley <chartley1988@gmail.com>
2025-02-11 12:25:58 -0800

    feat(HomePage): adds cards and bigger h1 to home page

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
 import "./components/navMenu/navMenu";
 import "./styles/style.css";
 import "./styles/theme.css";
+import "./styles/card.css";
+import { setTheme, redFelt } from "./components/table/themes";
+import StandardDeckOfCards from "./components/card/playingCard/standardDeckOfCards";
+
+const app = document.getElementById("app");
+if (app) {
+  setTheme(redFelt);
+
+  const deck = StandardDeckOfCards();
+  const dealer = deck.createPileElement("dealer", deck.cards);
+
+  dealer.cascade();
+  dealer.createCascadeLayout("spread", [0.4, 0]);
+  dealer.applyCascadeLayout("spread");
+  dealer.cascade();
+  console.log(dealer.cascadeOffset);
+
+  const section1 = document.createElement("section");
+  app.appendChild(section1);
+  section1.appendChild(dealer.container);
+}
